4ca0dc58a4e18ceff5a74156ed329a6b67e724ca
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2023-09-29 20:19:39 +0300

    Mise en place front

---
+++ Front/src/App.tsx
@@ -0,0 +1,23 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  // let items = [
+  //   "Jonathan",
+  //   "Joseph",
+  //   "Jotaro",
+  //   "Josuke",
+  //   "Giorno",
+  //   "Jolyne",
+  //   "Johnny",
+  //   "Gappy",
+  // ];
+  // return <div><ListGroup items={items} heading="Jojos"></ListGroup></div>;
+  return <>
+    <Header></Header>
+    <Footer></Footer>
+  </>
+}
+
+export default App;
