4ae4b3067f0ad3c8bb69cf8ed88843916f90081f
henintsoa454 <henintsoa404@gmail.com>
2023-10-07 11:05:43 +0330

    daySixth

--- Back/src/java/model/TestQuestionnaire.java
+++ Back/src/java/model/TestQuestionnaire.java
@@ -4,6 +4,11 @@
  */
 package model;
 
+import dbAccess.ConnectTo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author Henintsoa & Hery
@@ -11,7 +16,7 @@ package model;
 public class TestQuestionnaire {
     private int id;
     private int besoin;
-    private int question;
+    private String question;
     private boolean estchoixmultiple;
     private double note;
 
@@ -31,11 +36,11 @@ public class TestQuestionnaire {
         this.besoin = besoin;
     }
 
-    public int getQuestion() {
+    public String getQuestion() {
         return question;
     }
 
-    public void setQuestion(int question) {
+    public void setQuestion(String question) {
         this.question = question;
     }
 
@@ -58,12 +63,44 @@ public class TestQuestionnaire {
     public TestQuestionnaire() {
     }
 
-    public TestQuestionnaire(int id, int besoin, int question, boolean estchoixmultiple, double note) {
+    public TestQuestionnaire(int id, int besoin, String question, boolean estchoixmultiple, double note) {
         this.setId(id);
         this.setBesoin(besoin);
         this.setQuestion(question);
         this.setEstchoixmultiple(estchoixmultiple);
         this.setNote(note);
+    }
+    
+    public static void ajoutQuestionnaire(int besoin,String question,boolean estchoixmultiple,double note)throws Exception{
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        String query = "insert into testQuestionnaire(besoin,question,estchoixmultiple,note)values(?,?,?,?)";
+        try {
+            connection = ConnectTo.postgreS();
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1,besoin);
+            preparedStatement.setString(2,question);
+            preparedStatement.setBoolean(3,estchoixmultiple);
+            preparedStatement.setDouble(4,note);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {//2860
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
     
 }
